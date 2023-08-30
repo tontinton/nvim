f3139238d2537b904dbff0e25f586b3aaf37a516
Tony Solomonik <tony.solomonik@gmail.com>
2023-08-31 00:17:33 +0300

    jumplist: Add <n GT 1>j/k to jumplist

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -48,10 +48,10 @@ M.general = {
     -- http://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
     -- empty mode is same as using <cmd> :map
     -- also don't use g[j|k] when in operator pending mode, so it doesn't alter d, y or c behaviour
-    ["j"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", opts = { expr = true } },
-    ["k"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", opts = { expr = true } },
-    ["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', "Move up", opts = { expr = true } },
-    ["<Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", opts = { expr = true } },
+    ["j"] = { '(v:count > 1 ? "m\'" . v:count : "") . (v:count || mode(1)[0:1] == "no" ? "j" : "gj")', "Move down", opts = { expr = true } },
+    ["k"] = { '(v:count > 1 ? "m\'" . v:count : "") . (v:count || mode(1)[0:1] == "no" ? "k" : "gk")', "Move up", opts = { expr = true } },
+    ["<Up>"] = { '(v:count > 1 ? "m\'" . v:count : "") . (v:count || mode(1)[0:1] == "no" ? "k" : "gk")', "Move up", opts = { expr = true } },
+    ["<Down>"] = { '(v:count > 1 ? "m\'" . v:count : "") . (v:count || mode(1)[0:1] == "no" ? "j" : "gj")', "Move down", opts = { expr = true } },
 
     -- new buffer
     ["<C-n>"]      = { "<cmd> enew <CR>", "New buffer" },
