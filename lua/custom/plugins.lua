881470abd2fe919ebddb6951b61a0476b5fbd8ad
Tony Solomonik <tony.solomonik@gmail.com>
2023-11-11 15:20:01 +0200

    Add `mini.surround`

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -132,6 +132,7 @@ local plugins = {
       require('mini.bracketed').setup()
       require('mini.files').setup()
       require('mini.move').setup()
+      require('mini.surround').setup()
 
       local clue = require('mini.clue')
       clue.setup({
