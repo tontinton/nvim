91d4017d50185073a31a98e5114146af1ba18bf8
Tony Solomonik <tony.solomonik@gmail.com>
2023-12-29 20:56:18 +0200

    mappings: Live grep with args as lowercase

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -310,8 +310,8 @@ M.telescope = {
     -- find
     ["<leader>ff"] = { "<cmd> Telescope find_files <CR>", "Find files" },
     ["<leader>fa"] = { "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>", "Find all" },
-    ["<leader>fw"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
-    ["<leader>fW"] = { "<cmd> Telescope live_grep_args <CR>", "Live grep (args)" },
+    ["<leader>fw"] = { "<cmd> Telescope live_grep_args <CR>", "Live grep (args)" },
+    ["<leader>fW"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
     ["<leader>fb"] = { "<cmd> Telescope buffers <CR>", "Find buffers" },
     ["<leader>,"]  = { "<cmd> Telescope buffers <CR>", "Find buffers" },
     ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "Help page" },
