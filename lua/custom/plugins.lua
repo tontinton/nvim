bfda8b35b40e4a88d0e0c11c09979e5f55078222
Tony Solomonik <tony.solomonik@gmail.com>
2025-04-12 15:06:30 +0300

    lspconfig: Comment out java

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -73,19 +73,19 @@ local plugins = {
       },
     },
     config = function()
-      require('java').setup({
-        root_markers = {
-          'settings.gradle',
-          'settings.gradle.kts',
-          -- 'pom.xml',
-          'build.gradle',
-          'mvnw',
-          'gradlew',
-          'build.gradle',
-          'build.gradle.kts',
-          '.git',
-        },
-      })
+      -- require('java').setup({
+      --   root_markers = {
+      --     'settings.gradle',
+      --     'settings.gradle.kts',
+      --     -- 'pom.xml',
+      --     'build.gradle',
+      --     'mvnw',
+      --     'gradlew',
+      --     'build.gradle',
+      --     'build.gradle.kts',
+      --     '.git',
+      --   },
+      -- })
 
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
@@ -402,28 +402,28 @@ local plugins = {
     end,
   },
 
-  {
-    'nvim-java/nvim-java',
-    dependencies = {
-      'nvim-java/nvim-java-refactor',
-      'nvim-java/lua-async-await',
-      'nvim-java/nvim-java-core',
-      'nvim-java/nvim-java-test',
-      'nvim-java/nvim-java-dap',
-      'MunifTanjim/nui.nvim',
-      'neovim/nvim-lspconfig',
-      'mfussenegger/nvim-dap',
-      {
-        'williamboman/mason.nvim',
-        opts = {
-          registries = {
-            'github:nvim-java/mason-registry',
-            'github:mason-org/mason-registry',
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   'nvim-java/nvim-java',
+  --   dependencies = {
+  --     'nvim-java/nvim-java-refactor',
+  --     'nvim-java/lua-async-await',
+  --     'nvim-java/nvim-java-core',
+  --     'nvim-java/nvim-java-test',
+  --     'nvim-java/nvim-java-dap',
+  --     'MunifTanjim/nui.nvim',
+  --     'neovim/nvim-lspconfig',
+  --     'mfussenegger/nvim-dap',
+  --     {
+  --       'williamboman/mason.nvim',
+  --       opts = {
+  --         registries = {
+  --           'github:nvim-java/mason-registry',
+  --           'github:mason-org/mason-registry',
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 
   {
     "folke/neodev.nvim",
